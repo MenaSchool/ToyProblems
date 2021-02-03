b63d097f07395417f409f87d002e31b63733a503
wafaamurr <wafaamur55@gmail.com>
2021-02-03 10:26:58 -0800

    new version of ourcode

--- firstToyProblem.js
+++ firstToyProblem.js
@@ -7,3 +7,16 @@
 function minimum(array) {
   return;
 }
+
+var arr= [1,10,5,-3,100];
+var max=[0];
+for(var i =0 ; i<arr.length; i++){
+
+  if (arr[i]>=max){
+
+    max=arr[i];
+  }
+
+}
+console.log(max);
+console.log("hello");
