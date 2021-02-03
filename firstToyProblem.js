e07006f7c494af9ea17f28dbf9ab679fedfd68b6
mmahareeq <mmjaryq@gmail.com>
2021-02-03 22:05:21 +0200

    solve problem

--- firstToyProblem.js
+++ firstToyProblem.js
@@ -3,7 +3,21 @@
  * create a function that finds the minimum
  * without using any pre build in function
  */
-
+ // use for loop 
 function minimum(array) {
-  return;
+    let mini = array[0];
+    for(let i = 1 ; i < array.length ;i++)
+    {
+         if(mini > array[i])
+         {
+           mini = array[i]; 
+         }
+    }
+  return  mini;
 }
+
+
+let array = [1,10,5,-3,100];
+let nummin = minimum(array);
+
+console.log(nummin);
