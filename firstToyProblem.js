1a2503b59764f5b27cfa45df861b8ae100e25c07
MUSAMEH-97 <musameh.97@gmail.com>
2021-02-03 21:19:15 +0200

    Done

--- firstToyProblem.js
+++ firstToyProblem.js
@@ -4,6 +4,31 @@
  * without using any pre build in function
  */
 
+const myArr = [1, 10, 5, -3, 100];
+
+//function to return minValue of array
 function minimum(array) {
-  return;
+  let minValue = array[0];
+  for (let count = 1; count < array.length; count++) {
+    if (array[count] < minValue) {
+      minValue = array[count];
+    }
+  }
+  return minValue;
 }
+//function to return maxValue of array
+
+/*
+function maximum(array) {
+  let maxValue = array[0];
+  for (let count = 1; count < array.length; count++) {
+    if (array[count] > maxValue) {
+      maxValue = array[count];
+    }
+  }
+  return maxValue;
+}
+console.log(maximum(myArr));
+*/
+
+console.log(minimum(myArr));
