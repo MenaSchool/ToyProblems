e74a738f9c3f9192babdcd8bef2936e1a3bd26cc
Mahmoud-Nwiry <mamo992001@gmail.com>
2021-02-04 00:13:53 +0200

    my solution

--- firstToyProblem.js
+++ firstToyProblem.js
@@ -4,6 +4,16 @@
  * without using any pre build in function
  */
 
+ const arr = [1,10,5,-3,100];
+
 function minimum(array) {
-  return;
+  let num = array[0];
+  for(let a = 0;a<array.length;a++){
+    if(array[a] < num )
+      num = array[a];
+  }
+  return num;
 }
+
+
+console.log(minimum(arr));
