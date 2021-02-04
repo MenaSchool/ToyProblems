246e2b4493a60adbe9961571304e4b04ab6a3474
HassanGhazy <arabsama2017@gmail.com>
2021-02-04 16:14:49 +0200

    solve firstToyProblem

--- firstToyProblem.js
+++ firstToyProblem.js
@@ -1,9 +1,10 @@
-/**
- * [1,10,5,-3,100]
- * create a function that finds the minimum
- * without using any pre build in function
- */
 
 function minimum(array) {
-  return;
+  let min = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < min) {
+      min = array[i];
+    }
+  }
+  return min;
 }
