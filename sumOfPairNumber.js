73b2ce0c0b7f4a10a13fe74bc74b745bd9252fdb
KhaledMohammed <khaled.1m2h3o@gmail.com>
2021-02-04 22:49:51 +0300

    Update sumOfPairNumber.js

--- sumOfPairNumber.js
+++ sumOfPairNumber.js
@@ -6,34 +6,30 @@
  * sumPair([2,4,9,73])=>6
  */
 function sumPair(array) {
-
     let total = 0
-
-        if(Array.isArray(array)){  // for array
-            for(i of array){
-                if(i % 2 == 0){
-                    total = total + i
-                }
+    if(Array.isArray(array)){  // for array
+        for(i of array){
+            if(i % 2 == 0){
+                total = total + i
             }
-            return total
-        }else if(typeof array == "object"){ // can replace it by if(true) it works too     for object
-            for(let key in array){
-                if(array[key] % 2 == 0){
-                    total = total + array[key]
-                        };
-                    }
-            return total
-                }  
+        }
+        return total
+    }else if(typeof array == "object"){ // can replace it by if(true) it works too     for object
+        for(let key in array){
+            if(array[key] % 2 == 0){
+                total = total + array[key]
+            };
+        }
+        return total
+    }
 }
-
-    
-    console.log(sumPair([1,6,100,346,761,249]));
-    console.log(sumPair({
-           a:2,
-           b:5,
-           c:8,
-           d:10
-    }));
+console.log(sumPair([1,6,100,346,761,249]));
+console.log(sumPair({
+         a:2,
+         b:5,
+         c:8,
+         d:10
+}));
   /**
    * bonus points
    *
