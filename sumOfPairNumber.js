76f7e034acb3b3bd2d619d4c36712d2d22a06450
Ibrahim Mattar <ibmmtr@gmail.com>
2021-02-05 01:25:12 +0200

    Update sumOfPairNumber.js

--- sumOfPairNumber.js
+++ sumOfPairNumber.js
@@ -1,39 +1,28 @@
-/*
- * write a function that returns the sum of the pair numbers inside an array
- *
- * exple sumPair([1,6,100,346,761,249])=>452
- *
- * sumPair([2,4,9,73])=>6
- */
-function sumPair(array) {
 
-    let total = 0
+let Arr1 = [1, 24, 3, 4, 5, 6,"C", 73, 8, 19];
+let Arr2 = {a:1, b:24, c:3, d:4, e:5, f:6, g:73, h:8, i:19};
 
-        if(Array.isArray(array)){  // for array
-            for(i of array){
-                if(i % 2 == 0){
-                    total = total + i
-                }
+function sumEven(array) {
+  let sum = 0
+    if(Array.isArray(array)){  
+         for(i of array){
+           if(i % 2 === 0){	                
+             sum = sum + i
+             }
             }
-            return total
-        }else if(typeof array == "object"){ // can replace it by if(true) it works too     for object
-            for(let key in array){
-                if(array[key] % 2 == 0){
-                    total = total + array[key]
-                        };
-                    }
-            return total
-                }  
+             return sum;
+            }
+    else if (typeof array ==="object") {
+        for (let key in array){
+          if (array[key] % 2 === 0){             
+            (sum = sum + array[key])
+          }
+        }
+      return sum; 
+  }
 }
-
-    
-    console.log(sumPair([1,6,100,346,761,249]));
-    console.log(sumPair({
-           a:2,
-           b:5,
-           c:8,
-           d:10
-    }));
+  console.log(sumEven(Arr1));
+  console.log(sumEven(Arr2));
   /*
    * bonus points
    *
