24b907b71351f17db08878b2bf165d28c2fecdf7
mohamedubeid <abomuin112@hotmail.com>
2021-02-04 02:17:01 +0200

    Solved Proplem

--- firstToyProblem.js
+++ firstToyProblem.js
@@ -5,5 +5,13 @@
  */
 
 function minimum(array) {
-  return;
+  let min = array[0];
+  for(let i = 1; i < array.length; i++){
+    if(array[i] < min){
+      min = array[i];
+    }
+  }
+  return min;
 }
+const array = [1,10,5,-3,100];
+console.log(minimum(array));
