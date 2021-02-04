41a20e1b450efdb1dcfdc00e332c7649e768d47f
mohanadft <mohanadfteha@gmail.com>
2021-02-04 11:59:16 -0800

    first sol

--- firstToyProblem.js
+++ firstToyProblem.js
@@ -4,6 +4,17 @@
  * without using any pre build in function
  */
 
+const numbers = [1,10,5,-3,100];
+
 function minimum(array) {
-  return;
+    let lowest = 0;
+    
+    for(let i = 0; i < array.length; i++){
+        if (array[i] < lowest){
+            lowest = array[i];
+        }
+    }
+  return lowest;
 }
+
+console.log(minimum(numbers));
