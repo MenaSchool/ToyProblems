34f02071d1699b9b09824596272c5b3368cf3ba0
MUSAMEH-97 <musameh.97@gmail.com>
2021-02-05 23:49:09 +0200

    sum of pairs number done

--- sumOfPairNumber.js
+++ sumOfPairNumber.js
@@ -5,8 +5,23 @@
  *
  * sumPair([2,4,9,73])=>6
  */
-function sumPair(array) {
-  //your code goes here
+function sumPair(elements) {
+  if (Array.isArray(elements) === true) {
+    let sum = 0;
+    for (let count = 0; count < elements.length; count++) {
+      if (elements[count] % 2 === 0)
+        sum += elements[count];
+    }
+    return sum;
+  } else {
+    let str =" "
+    for (const accessItem in elements) {
+      if (elements[accessItem] % 2 === 0) {
+        str+=accessItem+" & ";
+      }
+    }
+    return str+"have pair values";
+  }
 }
 
 /**
@@ -21,9 +36,23 @@ function sumPair(array) {
  * obj={
  *     a:2,
  *     b:5,
- *     c:8
+ *     c:8,
+ *     v:4,
+ *     r:33
  * }
  *
- * return => "a & c have pair values"
+ * return => "a & c & v have pair values"
  *
  */
+const myArr = [1, 6, 100, 346, 761, 249];
+let obj = {
+  a: 2,
+  b: 5,
+  c: 8,
+  v: 4,
+  r:33,
+  x:100
+}
+
+console.log(sumPair([1, 6, 100, 346, 761, 249]));
+console.log(sumPair(obj));
