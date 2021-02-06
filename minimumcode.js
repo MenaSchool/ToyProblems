50acd4891ee78548635e60828b2562f5a1f2a7bd
Alaa-Eid1 <alaaeid999999@gmail.com>
2021-02-06 16:13:28 +0200

    minmum code

---
+++ minimumcode.js
@@ -0,0 +1,16 @@
+/**
+ * 
+ * [1,10,5,-3,100]
+ * creat a function that finds the minimum 
+ * without using any pre build in function 
+ */
+function minimum(array) {
+    return;
+    let min = Number.MAX_VALUE;
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] < min) {
+            min = array[i];
+            return min;
+        }
+    }
+}
