2ce794904a6f03985278af7703f18b4a69e989f1
unknown <omabd2001@gmail.com>
2021-02-06 16:29:29 +0200

    update sumOfPairNumber

--- sumOfPairNumber.js
+++ sumOfPairNumber.js
@@ -1,3 +1,5 @@
+
+
 /**
  * write a function that returns the sum of the pair numbers inside an array
  *
@@ -5,24 +7,37 @@
  *
  * sumPair([2,4,9,73])=>6
  */
-console.log(sumPair([1,6,100,346,761,249]));
+console.log(sumPair({ a: 2, b: 5, c: 8 , k:20}));
 
-console.log(pair({ a: 2, b: 5, c: 8 }));
+// console.log(pair({ a: 2, b: 5, c: 8 }));
 
-
+    // console.log( Array.isArray({ a: 2, b: 5, c: 8 }))
 
 function sumPair(array) {
-  var sum = 0;
-  for (var i = 0; i < array.length; i++){
-    if (array[i] % 2 == 0) {
-      sum += array[i];
+
+  if (Array.isArray(array)) {
+    var sum = 0;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i] % 2 == 0) {
+        sum += array[i];
+      }
+
     }
-
+    return sum;
+  } else if (typeof array == 'object') {
+    let pair = [];
+    for (var key in array) {
+      if (array[key] % 2 === 0) {
+        pair.push(key);
+      }
+    }
+    return pair.join(' & ') + ' have pair values';
+  }else
+    console.log("🤪🤪")
   }
-  return sum;
 
 
-}
+
 
 /**
  * bonus points
@@ -44,16 +59,3 @@ function sumPair(array) {
  * return => "a & c have pair values"
  *
  */
-function pair(array) {
-  var Keys = "";
-  for (var key in array) {
-   
-    if (array[key] % 2 == 0) {
-      Keys+= array[key]+","
-    }
-
-  }
-  return "The " +Keys +"have pair values"
-    
-    
-}
